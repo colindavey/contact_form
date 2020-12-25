303cb4f1de330d968ec8cc11d50a3517374fd50a
Colin Davey <himself@colindavey.com>
2020-12-25 15:33:04 -0800

    Add remaining fields and validation

--- contact_form.php
+++ contact_form.php
@@ -1,16 +1,50 @@
 <?php
-$name_msg = "";
-$success = false;
-// if(isset($_POST["submitted"])) 
+$name_warning = "";
+$email_warning = "";
+$subject_warning = "";
+$message_warning = "";
+$name = "";
+$email = "";
+$subject = "";
+$message = "";
 if ($_SERVER["REQUEST_METHOD"] === "POST")
 { 
     $success = true;
-    if ($_POST["name"] === "") {
-        $name_msg = "Required";
+    $_POST = array_map("trim", $_POST);
+
+    $name = $_POST["name"];
+    if (empty($name)) {
+        $name_warning = "Required";
+        $success = false;
+    } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+        $name_warning = "Only letters, apostrophes, dashes and white space allowed";
+        $success = false;
+    }
+
+    $email = $_POST["email"];
+    if (empty($email)) {
+        $email_warning = "Required";
+        $success = false;
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $email_warning = "Invalid email format";
+        $success = false;
+    }
+
+    $subject = $_POST["subject"];
+    if (empty($subject)) {
+        $subject_warning = "Required";
+        $success = false;
+    }
+
+    $message = $_POST["message"];
+    if (empty($message)) {
+        $message_warning = "Required";
         $success = false;
     }
 }
+
 if ($success) {
     header("location: thank_you.php");
 }
-require("contact_html.php");
+
+require "contact_html.php";
--- contact_html.php
+++ contact_html.php
@@ -6,9 +6,27 @@
     <h1>Contact Colin Davey</h1>
     <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
         <label for="name" class="">Your name:</label>
-        <span id="name-warn" style="display:inline;color:red"> <?= $name_msg ?></span>
+        <span id="name-warn" style="display:inline;color:red"> <?= $name_warning ?></span>
         <br>
-        <input type="text" id="name" name="name" class="form-control">
+        <input type="text" id="name" name="name" class="form-control" value="<?= $name ?>">
+        <br><br>
+
+        <label for="email" class="">Your email address:</label>
+        <span id="emal-warn" style="display:inline;color:red"> <?= $email_warning ?></span>
+        <br>
+        <input type="text" id="email" name="email" class="form-control" value="<?= $email ?>">
+        <br><br>
+
+        <label for="subject" class="">Subject:</label>
+        <span id="subject-warn" style="display:inline;color:red"> <?= $subject_warning ?></span>
+        <br>
+        <input type="text" id="subject" name="subject" class="form-control" value="<?= $subject ?>">
+        <br><br>
+
+        <label for="message" class="">Message:</label>
+        <span id="message-warn" style="display:inline;color:red"> <?= $message_warning ?></span>
+        <br>
+		<textarea type="text" id="message" name="message" rows="3" value="<?= $message ?>"></textarea>
         <br><br>
 
         <button type="submit">Submit</button>
