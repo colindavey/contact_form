cf2010d4788c13aebca64028251fb30c834d7e65
Colin Davey <himself@colindavey.com>
2020-12-25 11:14:51 -0800

    Added single input, validataion, and redirect on success

--- contact_form.php
+++ contact_form.php
@@ -1,8 +1,16 @@
 <?php
-$message = "BEFORE";
+$name_msg = "";
+$success = false;
 // if(isset($_POST["submitted"])) 
 if ($_SERVER["REQUEST_METHOD"] === "POST")
 { 
-    $message = "AFTER";
+    $success = true;
+    if ($_POST["name"] === "") {
+        $name_msg = "Required";
+        $success = false;
+    }
+}
+if ($success) {
+    header("location: thank_you.php");
 }
 require("contact_html.php");
--- contact_html.php
+++ contact_html.php
@@ -3,9 +3,14 @@
     <title>Document</title>
 </head>
 <body>
-    <h1><?= $message ?></h1>
+    <h1>Contact Colin Davey</h1>
     <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <!-- <button type="submit" name="submitted" value="yes">Submit</button> -->
+        <label for="name" class="">Your name:</label>
+        <span id="name-warn" style="display:inline;color:red"> <?= $name_msg ?></span>
+        <br>
+        <input type="text" id="name" name="name" class="form-control">
+        <br><br>
+
         <button type="submit">Submit</button>
     </form>
 </body>
