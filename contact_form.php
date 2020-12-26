dabf92a5e60200e42ab66a2fbac8f322d5f1700f
Colin Davey <himself@colindavey.com>
2020-12-26 11:04:13 -0800

    Streamlined code

--- contact_form.php
+++ contact_form.php
@@ -3,48 +3,67 @@ $name_warning = $email_warning = $subject_warning = $message_warning = "";
 $name = $email = $subject = $message = "";
 if ($_SERVER["REQUEST_METHOD"] === "POST")
 { 
-    $success = true;
-    $_POST = array_map("trim", $_POST);
-
-    list($name, $name_warning) = check_required_post_var("name");
-    if (!empty($name_warning)) {
-        $success = false;
-    } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-        echo "in else<br>";
-        $name_warning = "Only letters, apostrophes, dashes and white space allowed";
-        $success = false;
+    // Doing trim in loop below instead.
+    // $_POST = array_map("trim", $_POST);
+    // Note: Map isn't able to create an associate array. There is a trick for doing it with reduce,
+    // but this loop seems clearer. 
+    // https://stackoverflow.com/questions/11563119/how-to-convert-an-array-of-arrays-or-objects-to-an-associative-array
+    $result = array();
+    foreach(["name", "email", "subject", "message"] as $var_name) {
+        $value = trim($_POST[$var_name]);
+        $result[$var_name] = array("value" => $value, "warning" => empty($value) ? "Required" : "");
     }
 
-    list($email, $email_warning) = check_required_post_var("email");
-    if (!empty($email_warning)) {
-        $success = false;
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $email_warning = "Invalid email format";
-        $success = false;
+    // If there is a name, check that it is valid
+    if (empty($result["name"]["warning"])) {
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $result["name"]["value"])) {
+            $result["name"]["warning"] = "Only letters, apostrophes, dashes and white space allowed";
+        }
     }
 
-    list($subject, $subject_warning) = check_required_post_var("subject");
-    if (!empty($subject_warning)) {
-        $success = false;
+    // If there is an email, check that it is valid
+    if (empty($result["email"]["warning"])) {
+        if (!filter_var($result["email"]["value"], FILTER_VALIDATE_EMAIL)) {
+            $result["email"]["warning"] = "Invalid email format";
+        }
     }
 
-    list($message, $message_warning) = check_required_post_var("message");
-    if (!empty($message_warning)) {
-        $success = false;
+    // Success means there are no warnings, ie, the warnings are all empty
+    // Which is better? 
+    $success = array_reduce($result, "is_success", true);
+    // $success = array_reduce(
+    //     $result, 
+    //     function ($carry, $item) {
+    //         return $carry && empty($item["warning"]);
+    //     },
+    //     true
+    // );
+
+    // Redirect to the thank-you page if successful
+    if ($success) {
+        header("location: thank_you.php");
     }
+    // These assignments are so the html piece doesn't need longer array expressions.
+    $name_warning = $result["name"]["warning"];
+    $email_warning = $result["email"]["warning"];
+    $subject_warning = $result["subject"]["warning"];
+    $message_warning = $result["message"]["warning"];
+
+    $name = $result["name"]["value"];
+    $email = $result["email"]["value"];
+    $subject = $result["subject"]["value"];
+    $message = $result["message"]["value"];
 }
-
-// Redirect to the thank-you page if successful
-if ($success) {
-    header("location: thank_you.php");
-}
-
 require "contact_html.php";
 
-function check_required_post_var($var_name) {
-    $value = $_POST[$var_name];
-    return array(
-        $value, 
-        empty($value) ? "Required" : ""
-    );
+function is_success($carry, $item) {
+    return $carry && empty($item["warning"]);
+}
+
+function dd($var, $do_die=true) {
+    var_dump($var);
+    echo "<br>";
+    if ($do_die) {
+        die();
+    }
 }
--- contact_html.php
+++ contact_html.php
@@ -4,11 +4,13 @@
 </head>
 <body>
     <h1>Contact Colin Davey</h1>
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+    <!-- <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>"> -->
+    <form method="post" >
         <label for="name" class="">Your name:</label>
         <span id="name-warn" style="display:inline;color:red"> <?= $name_warning ?></span>
         <br>
         <input type="text" id="name" name="name" class="form-control" value="<?= $name ?>">
+        <!-- <input type="text" id="name" name="name" class="form-control"> -->
         <br><br>
 
         <label for="email" class="">Your email address:</label>
