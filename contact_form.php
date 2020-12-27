586963836accd05d0d6abde1f18dee190d392259
Colin Davey <himself@colindavey.com>
2020-12-26 16:22:57 -0800

    Added robot-checkbox handling

--- contact_form.php
+++ contact_form.php
@@ -1,15 +1,20 @@
 <?php
-$name_warning = $email_warning = $subject_warning = $message_warning = "";
-$name = $email = $subject = $message = "";
+// $name_warning = $email_warning = $subject_warning = $message_warning = "";
+// $name = $email = $subject = $message = "";
+// $robot_warning = "";
+// $robot_check = "";
+// $not_robot_check = "";
 if ($_SERVER["REQUEST_METHOD"] === "POST")
 { 
     // Doing trim in loop below instead.
     // $_POST = array_map("trim", $_POST);
-    // Note: Map isn't able to create an associate array. There is a trick for doing it with reduce,
-    // but this loop seems clearer. 
+
+    // Note: array_map() isn't able to create an associate array. There is a trick for doing it
+    // with array_reduce(), but this loop seems clearer. 
     // https://stackoverflow.com/questions/11563119/how-to-convert-an-array-of-arrays-or-objects-to-an-associative-array
     $result = array();
     foreach(["name", "email", "subject", "message"] as $var_name) {
+        // Does this need stripslashes() or htmlspecialchars()?
         $value = trim($_POST[$var_name]);
         $result[$var_name] = array("value" => $value, "warning" => empty($value) ? "Required" : "");
     }
@@ -29,20 +34,23 @@ if ($_SERVER["REQUEST_METHOD"] === "POST")
     }
 
     // Success means there are no warnings, ie, the warnings are all empty
-    // Which is better? 
+    // Calculate success based on the above
     $success = array_reduce($result, "is_success", true);
-    // $success = array_reduce(
-    //     $result, 
-    //     function ($carry, $item) {
-    //         return $carry && empty($item["warning"]);
-    //     },
-    //     true
-    // );
+
+    // Handle the "are you a robot" checkboxes
+    list($robot_bool, $robot_check) = get_check("robot_check");
+    list($not_robot_bool, $not_robot_check) = get_check("not_robot_check");
+    $robot_success = !$robot_bool && $not_robot_bool;
+    $robot_warning = !$robot_success ? "Robots may not contact Colin Davey" : "";
+
+    // Add "are you a robot" to the success calculation
+    $success = $success && $robot_success;
 
     // Redirect to the thank-you page if successful
     if ($success) {
         header("location: thank_you.php");
     }
+
     // These assignments are so the html piece doesn't need longer array expressions.
     $name_warning = $result["name"]["warning"];
     $email_warning = $result["email"]["warning"];
@@ -58,6 +66,11 @@ require "contact_html.php";
 
 function is_success($carry, $item) {
     return $carry && empty($item["warning"]);
+}
+
+function get_check($var_name) {
+    $bool = isset($_POST[$var_name]);
+    return array($bool , $bool ? "checked" : "");
 }
 
 function dd($var, $do_die=true) {
--- contact_html.php
+++ contact_html.php
@@ -6,29 +6,43 @@
     <h1>Contact Colin Davey</h1>
     <!-- <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>"> -->
     <form method="post" >
-        <label for="name" class="">Your name:</label>
+        <label for="name" >Your name:</label>
         <span id="name-warn" style="display:inline;color:red"> <?= $name_warning ?></span>
         <br>
-        <input type="text" id="name" name="name" class="form-control" value="<?= $name ?>">
+        <input type="text" id="name" name="name" value="<?= $name ?>">
         <!-- <input type="text" id="name" name="name" class="form-control"> -->
         <br><br>
 
-        <label for="email" class="">Your email address:</label>
+        <label for="email">Your email address:</label>
         <span id="emal-warn" style="display:inline;color:red"> <?= $email_warning ?></span>
         <br>
-        <input type="text" id="email" name="email" class="form-control" value="<?= $email ?>">
+        <input type="text" id="email" name="email" value="<?= $email ?>">
         <br><br>
 
-        <label for="subject" class="">Subject:</label>
+        <label for="subject">Subject:</label>
         <span id="subject-warn" style="display:inline;color:red"> <?= $subject_warning ?></span>
         <br>
-        <input type="text" id="subject" name="subject" class="form-control" value="<?= $subject ?>">
+        <input type="text" id="subject" name="subject" value="<?= $subject ?>">
         <br><br>
 
-        <label for="message" class="">Message:</label>
+        <label for="message">Message:</label>
         <span id="message-warn" style="display:inline;color:red"> <?= $message_warning ?></span>
         <br>
-		<textarea type="text" id="message" name="message" rows="3" class="form-control"><?= $message ?></textarea>
+		<textarea type="text" id="message" name="message" rows="3"><?= $message ?></textarea>
+        <br><br>
+
+        <fieldset style="width:10em">
+            <legend>Please check the boxes that apply:</legend>
+            <div>
+                <input type="checkbox" value="yes" name="robot_check" id="robot_check" <?= $robot_check ?>>
+                <label for="robot-check">I am a robot.</label>
+            </div>
+            <div>
+                <input type="checkbox" value="yes" name="not_robot_check" id="not_robot_check" <?= $not_robot_check ?>>
+                <label for="not_robot_check">I am a not robot.</label>
+            </div>
+            </fieldset>
+            <p id="robot-warn" style="color:red"> <?= $robot_warning ?></p>
         <br><br>
 
         <button type="submit">Submit</button>
